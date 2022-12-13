6840679f278af149175c0c844709fdd362e2ecb6
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2022-12-13 19:00:02 -0300

    feat: :sparkles: Adiciona http-status-codes

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,14 +1,9 @@
 import express from 'express'
+import {router} from './routes'
 
 const server = express()
 
-server.get('/', (_, res) => {
-
-	return res.send('Hello world!')
-})
-
-interface Teste {
-
-}
+server.use(express.json())
+server.use(router)
 
 export { server }
