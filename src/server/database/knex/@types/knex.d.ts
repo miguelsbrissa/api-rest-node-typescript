5f7a9be027086a6c92b7ca104afba7254fd58965
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2022-12-17 20:56:03 -0300

    feat: :sparkles: Cria model de cidade e a tipagem do Knex

--- src/server/database/knex/@types/knex.d.ts
+++ src/server/database/knex/@types/knex.d.ts
@@ -1,6 +1,7 @@
+import { ICidade } from '../../../models'
 declare module 'knex/types/tables'{
 	interface Tables{
-		// cidade: ICidade
+		cidade: ICidade
 		// pessoa: IPessoa
 		// usuario: IUsuario
 	}
