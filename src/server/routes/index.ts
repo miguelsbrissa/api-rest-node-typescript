ea61180c40194344726a6aa698c6b3dee606075f
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-02-07 14:27:35 -0300

    feat: :sparkles: Adiciona rota de pessoas

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import 'dotenv/config'
-import { CidadeController } from '../controllers'
+import { CidadeController, PessoaController } from '../controllers'
 
 
 const router = Router()
@@ -17,5 +17,11 @@ router.get('/cidades', CidadeController.getAllValidation, CidadeController.getAl
 router.get('/cidades/:id', CidadeController.getByIdValidation, CidadeController.getById)
 router.put('/cidades/:id', CidadeController.updateByIdValidation, CidadeController.updateById)
 router.delete('/cidades/:id', CidadeController.deleteByIdValidation, CidadeController.deleteById)
+
+router.post('/pessoas', PessoaController.createValidation, PessoaController.create)
+router.get('/pessoas', PessoaController.getAllValidation, PessoaController.getAll)
+router.get('/pessoas/:id', PessoaController.getByIdValidation, PessoaController.getById)
+router.put('/pessoas/:id', PessoaController.updateByIdValidation, PessoaController.updateById)
+router.delete('/pessoas/:id', PessoaController.deleteByIdValidation, PessoaController.deleteById)
 
 export { router }
