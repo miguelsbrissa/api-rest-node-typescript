0a229bcb22c416768e94c44e1b344fb699a3ca59
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-02-07 19:19:39 -0300

    feat: :sparkles: Adiciona model e migration de Usuario

---
+++ src/server/database/migrations/0002_create_usuario.ts
@@ -0,0 +1,19 @@
+import { Knex } from 'knex'
+import { ETableNames } from '../ETableNames'
+
+export async function up(knex: Knex) {
+	return knex.schema.createTable(ETableNames.usuario, table => {
+		table.bigIncrements('id').primary().index()
+		table.string('nome').checkLength('>', 3).notNullable()
+		table.string('email').index().unique().checkLength('>', 5).notNullable()
+		table.string('senha').notNullable()
+	}).then(() => {
+		console.log(`# Create table ${ETableNames.usuario}`)
+	})
+}
+
+export async function down(knex: Knex) {
+	return knex.schema.dropTable(ETableNames.usuario).then(() => {
+		console.log(`# Drop table ${ETableNames.usuario}`)
+	})
+}
